e3d9269be77165234c06061c2e5c1f5067588422
losp <ginolospazzino@hotmail.com>
2021-09-16 14:57:53 +0200

    My visitor class

---
+++ src/MyVisitor.java
@@ -0,0 +1,22 @@
+public class MyVisitor extends MicroCBaseVisitor {
+
+    @Override public Object visitProgram(MicroCParser.ProgramContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitStatement(MicroCParser.StatementContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitLexpr(MicroCParser.LexprContext ctx) { return visitChildren(ctx); }
+   
+    @Override public Object visitRexpr(MicroCParser.RexprContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitArray(MicroCParser.ArrayContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitBexpr(MicroCParser.BexprContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitDecl(MicroCParser.DeclContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitOpa(MicroCParser.OpaContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitOpr(MicroCParser.OprContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitOpb(MicroCParser.OpbContext ctx) { return visitChildren(ctx); }
+}
