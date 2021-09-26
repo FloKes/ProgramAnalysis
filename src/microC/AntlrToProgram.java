548608c2ac3825285490dc689ddf15410cb440d0
losp <ginolospazzino@hotmail.com>
2021-09-26 20:10:06 +0200

    VisitProgram method

--- src/microC/AntlrToProgram.java
+++ src/microC/AntlrToProgram.java
@@ -9,6 +9,13 @@ public class AntlrToProgram extends MicroCBaseVisitor<Program> {
     public Program visitProgram(MicroCParser.ProgramContext ctx) {
         Program prog = new Program();
 
+        for(int i = 0; i < ctx.getChildCount(); i++) {
+            if(i == ctx.getChildCount() - 1) { //last child is EOF. Do not visit this child
+
+            } else {
+                prog.addExpression((Rexpression) ctx.getChild(i)); //TODO: we need a class which is more general. Expression class
+            }
+        }
         return prog;
     }
 }
